4b340d8ce23e810b3d4f900125b1173467d3a878
James Benner <hello@jamesbenner.com>
2025-10-06 20:50:10 -0400

    Implement library

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-export default function (): string {
-  return 'Hello world!';
+import fs from 'node:fs';
+import { open } from 'node:fs/promises';
+import type { FileHandle } from 'node:fs/promises';
+
+const { S_IXUSR, S_IXGRP, S_IXOTH } = fs.constants;
+
+export async function isExecutable(filepath: string): Promise<boolean> {
+  let file: FileHandle;
+
+  try {
+    file = await open(filepath);
+    const stats = await file.stat();
+
+    return stats.mode === (stats.mode | S_IXUSR | S_IXGRP | S_IXOTH);
+  } finally {
+    // @ts-expect-error File is defined if the try block is executed.
+    file?.close();
+  }
+}
+
+export function isExecutableSync(filepath: string): boolean {
+  const stats = fs.statSync(filepath);
+
+  return stats.mode === (stats.mode | S_IXUSR | S_IXGRP | S_IXOTH);
 }

7c41ab10413b08059c41a73424b3a43ffb41b6db
James Benner <hello@jamesbenner.com>
2025-10-06 20:48:43 -0400

    Update tests

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,8 +1,14 @@
-import { isExecutable } from '../src/index.js';
+import { isExecutable, isExecutableSync } from '../src/index.js';
 import { describe, expect, it } from 'vitest';
 
 describe('isExecutable', () => {
   it('is a function', () => {
     expect(isExecutable).toBeTypeOf('function');
+  });
+});
+
+describe('isExecutableSync', () => {
+  it('is a function', () => {
+    expect(isExecutableSync).toBeTypeOf('function');
   });
 });

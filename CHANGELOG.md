574914f497c84f623ef3c5bc79fd8ced96f1d78b
James Benner <hello@jamesbenner.com>
2025-10-17 14:53:51 -0400

    Add verbose flag to CLI app

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 [0.4.0] - 2025-10-17
 --------------------
 
+### Added
+
+- A `--verbose` flag to the CLI app for operational reporting.
+
 ### Changed
 
 - Visually refined the error reporting for the CLI app.
--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@
 
 import pkg from '../package.json' with { type: 'json' };
 import { isExecutable } from './index.js';
+import chalk from 'chalk';
 import logSymbols from 'log-symbols';
 import meow from 'meow';
 import path from 'node:path';
@@ -15,6 +16,7 @@ const cli = meow(`
 
   Options
     --help, -h     Display this message.
+    --verbose, -V  Display messages pertaining to each operation.
     --version, -v  Display the application version.
 `, {
   importMeta: import.meta,
@@ -22,6 +24,11 @@ const cli = meow(`
     help: {
       type: 'boolean',
       shortFlag: 'h'
+    },
+    verbose: {
+      default: false,
+      type: 'boolean',
+      shortFlag: 'V'
     },
     version: {
       type: 'boolean',
@@ -37,6 +44,22 @@ if (cli.input.length === 0) {
 try {
   const filepaths = cli.input.map(filepath => path.resolve(filepath));
   const results = await Promise.all(filepaths.map(filepath => isExecutable(filepath)));
+
+  if (cli.flags.verbose) {
+    results.forEach((result, index) => {
+      const filepath = filepaths[index]!;
+      const relativePath = path.relative(process.cwd(), filepath);
+
+      if (result) {
+        console.log(logSymbols.success, `File ${chalk.underline(relativePath)} is executable.`);
+      } else {
+        console.error(
+          logSymbols.error,
+          `File ${chalk.underline(relativePath)} is ${chalk.bold('NOT')} executable.`
+        );
+      }
+    });
+  }
 
   if (!results.every(Boolean)) {
     process.exit(1);
